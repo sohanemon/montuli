c86e070541ef5f19bd3c430658720e7b96730df7
sohanemon <m.sohan.rse@gmail.com>
2021-11-25 10:02:57 +0600

    ShowCase Ready

--- src/components/body/MenuItem.js
+++ src/components/body/MenuItem.js
@@ -1,6 +1,34 @@
 import React from "react";
+import { Card, CardTitle, CardBody, CardImg, CardImgOverlay } from "reactstrap";
 let MenuItem = (props) => {
   console.log(props);
-  return <div></div>;
+  return (
+    <div>
+      <Card style={{ margin: "10px" }}>
+        <CardBody>
+          <CardImg
+            width="100px"
+            height="400px"
+            alt={props.dish.name}
+            src={props.dish.image}
+            style={{ opacity: ".5" }}
+          />
+        </CardBody>
+        <CardImgOverlay>
+          <CardTitle>
+            <h3
+              style={{
+                marginTop: "35%",
+                fontWeight: "Bold",
+                color: "black",
+              }}
+            >
+              {props.dish.name}
+            </h3>
+          </CardTitle>
+        </CardImgOverlay>
+      </Card>
+    </div>
+  );
 };
 export default MenuItem;
