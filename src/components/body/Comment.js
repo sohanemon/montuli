18f5f83797b70c9ef46aa8636d344a4052b3d8de
sohanemon <m.sohan.rse@gmail.com>
2021-11-26 07:24:12 +0600

    gh pages

--- src/components/body/Comment.js
+++ src/components/body/Comment.js
@@ -1,11 +1,26 @@
 import React from "react";
 let Comment = (props) => {
   let allComment = props.com.map((ele) => {
+    let star = "";
+    for (let x = 0; x < ele.rating; x++) {
+      star = star + " *";
+    }
     return (
       <div key={ele.id}>
         <br />
-        <h5>Name: {ele.author}</h5>
-        <div>Rating: {ele.rating}</div>
+        <h5>{ele.author}</h5>
+        <div>
+          Rating:{" "}
+          <span
+            style={{
+              fontWeight: "700",
+              fontSize: "25px",
+              marginTop: "39px",
+            }}
+          >
+            {star}
+          </span>
+        </div>
         <div>{ele.comment}</div>
       </div>
     );
